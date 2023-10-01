cb0b803182c683ccdb60c27d6de222de40b52607
lorenzo-frittoli <lorenzo.frittoli.ge@gmail.com>
2023-10-01 15:46:41 +0200

    Made backup function. Made backup command.

---
+++ auto_backup_maker.py
@@ -0,0 +1,18 @@
+import time
+import os
+
+from constants import AUTO_BACKUPS_DIR, BACKUP_FREQ
+from manage_helpers import make_backup
+
+
+def main():
+    """Auto backs up the db
+    """        
+    while True:
+        make_backup(AUTO_BACKUPS_DIR)
+        time.sleep(BACKUP_FREQ)
+
+
+if __name__ == "__main__":
+    print("Ctrl-C to kill")
+    main()
