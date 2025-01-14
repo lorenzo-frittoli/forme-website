272c2183477ea865d5e0446a0990cae79e7ef411
BestCrazyNoob <lucabaglietto06@gmail.com>
2025-01-14 19:08:09 +0100

    remove dead code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, redirect, render_template, request, session, g, Response, abort, url_for
+from flask import Flask, redirect, render_template, request, session, g, Response, url_for
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 import json
