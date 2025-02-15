ef010bc0f0184dc6afc21d64b387ef506c503882
BestCrazyNoob <lucabaglietto06@gmail.com>
2025-02-15 20:35:23 +0100

    Permanent session login

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 # Configure session to use filesystem (instead of signed cookies)
 # I have no idea what this means just roll with it
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
