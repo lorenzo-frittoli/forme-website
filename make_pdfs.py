ad66ddfed26238bda701253070765da7f380ad1e
BestCrazyNoob <lucabaglietto06@gmail.com>
2025-03-07 22:04:53 +0100

    Update make_pdfs

--- make_pdfs.py
+++ make_pdfs.py
@@ -1,66 +1,60 @@
 from constants import *
 from helpers import fmt_timespan
+from jinja2 import Template
+import json
 import sqlite3
+
+TEMPLATE = "templates" + DIR_SEP + "pdfs.tex"
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+if not os.path.exists(TEX_DIRECTORY):
+    os.makedirs(TEX_DIRECTORY)
+
+with open(TEMPLATE, "r") as t_file:
+    template = Template(
+        t_file.read(),
+        line_statement_prefix="%-j-",
+    )
 
 con = sqlite3.connect(DATABASE)
 
-activities = con.execute("SELECT title, length, id FROM activities ORDER BY id;").fetchall()
+activities = con.execute("SELECT title, length, id, speakers FROM activities ORDER BY id;").fetchall()
 
 # Create a separate pdf for each day
 for day_index, day in enumerate(DAYS):
+    data = []
+
+    for activity_title, activity_length, activity_id, activity_speakers in activities:
+        activity_title = activity_title.replace('&', '\\&')
+        activity_speakers = activity_speakers.replace('&', '\\&')
+
+        bookings = []
+
+        for module_start in range(0, len(TIMESPANS)-activity_length+1, activity_length):
+            result = con.execute(
+                "SELECT full_name, class FROM users JOIN registrations ON users.id = registrations.user_id WHERE activity_id = ? AND day = ? AND module_start = ? ORDER BY full_name;",
+                (activity_id, day_index, module_start)
+            ).fetchall()
+
+            if len(result) % 2:
+                result.append(("", ""))
+
+            bookings.append([
+                fmt_timespan(module_start, module_start + activity_length - 1),
+                [(_class0 or "", full_name0, _class1 or "", full_name1) for (full_name0, _class0), (full_name1, _class1) in chunks(result, 2)],
+            ])
+
+        data.append([activity_title, activity_speakers, bookings])
+
     # File con '/' nel nome non sono validi
     with open(TEX_DIRECTORY + day.replace("/", "_")+".tex", "w", encoding="UTF-8") as outf:
-        def put(*args, **kwargs):
-            print(*args, **kwargs, file=outf)
-
-        put(
-"""\\documentclass{article}
-
-\\usepackage[italian]{babel}
-\\usepackage[a4paper,top=2cm,bottom=2cm,left=2cm,right=2cm,marginparwidth=1.75cm]{geometry}
-\\usepackage{array}
-
-% Set section numbers only for the first level (sections)
-\\setcounter{secnumdepth}{1}
-% Disable page numbers
-\\pagenumbering{gobble}
-
-\\begin{document}"""
-        )
-        for activity_title, activity_length, activity_id in activities:
-            put(
-f"""\\Large\\textbf{{Foglio firme - {DAYS_TEXT[day_index]}}}
-\\section{{{activity_title}}}"""
+        outf.write(
+            template.render(
+                day=DAYS_TEXT[day_index],
+                activities=data,
             )
-            for module_start in range(0, len(TIMESPANS)-activity_length+1, activity_length):
-                module_end = module_start + activity_length - 1
-                # Get the speakers
-                speakers = con.execute(
-                    "SELECT speakers FROM activities WHERE id = ?;",
-                    (activity_id, )
-                ).fetchone()[0]
-                # Double { } are not formatted by the f-string
-                put(
-f"""\\begin{{minipage}}{{100em}}
-\\subsection{{{fmt_timespan(module_start, module_end)}}}
-Relatori: {speakers} \\\\
-\\\\
-\\begin{{tabular}}{{| m{{7.2cm}} | m{{1.6cm}} | m{{7.2cm}} |}}
-\\hline"""
-                )
-                # Get the registrations from the database
-                result = con.execute(
-                    # Sort by type than by surname
-                    "SELECT full_name, class FROM users JOIN registrations ON users.id = registrations.user_id WHERE activity_id = ? AND day = ? AND module_start = ? ORDER BY type, full_name;",
-                    (activity_id, day_index, module_start)
-                )
-                for full_name, _class in result:
-                    # \\ : newline
-                    put(full_name, "&", _class if _class else "esterno", " & \\\\")
-                    put("\\hline")
-                put(
-"""\\end{tabular}
-\\end{minipage}"""
-)
-            put("\\newpage")
-        put("\\end{document}")
+        )
